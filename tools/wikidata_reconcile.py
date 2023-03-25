97d2391300b61f0886df140a83c18556a7eb44c3
Paul Balluff <paul.balluff@gmail.com>
2023-03-25 20:48:28 +0100

    #1 dataset for language entries

---
+++ tools/wikidata_reconcile.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from flaskinventory.auxiliary import icu_codes
+from reconciler import reconcile
+import requests
+import json
+from slugify import slugify
+
+icu = [{'iso': k, 'language': v} for k, v in icu_codes.items()]
+
+df = pd.DataFrame(icu)
+
+reconciled = reconcile(df["iso"], type_id="Q34770", property_mapping={"P218": df['iso']})
+
+# reconciled.to_csv('languages.csv')
+
+no_match = reconciled[reconciled.match == False]
+
+no_match.to_json('languages_nomatch.json', orient='records',indent=1)
+
+reconciled = reconciled[reconciled.match == True]
+
+
+api = 'https://www.wikidata.org/w/api.php'
+
+
+params = {'action': 'wbgetentities', #'languages': 'en',
+          'format': 'json'}
+
+languages = []
+
+for index, row in reconciled.iterrows():
+    print(row['name'])
+    wikidataid = row['id']
+    params['ids'] = wikidataid 
+    r = requests.get(api, params=params)
+    wikidata = r.json()
+    result = {'name': row['name'],
+              'wikidata_id': wikidataid,
+              'description': row['description'],
+              'icu_code': row['input_value']}
+    result['alternate_names'] = []
+    try:
+        result['name' + '@' + row['input_value']] = wikidata['entities'][wikidataid]['labels'][row['input_value']]['value'] # native label
+    except:
+        pass
+    result['name@en'] = wikidata['entities'][wikidataid]['labels']['en']['value']
+    aliases = wikidata['entities'][wikidataid]['aliases']['en']
+    for alias in aliases:
+        result['alternate_names'].append(alias['value'])
+    # P219 ISO 639-2; P220 ISO 639-3
+    try:
+        result['iso_639_2'] = wikidata['entities'][wikidataid]['claims']['P219'][0]['mainsnak']['datavalue']['value']
+        result['iso_639_3'] = wikidata['entities'][wikidataid]['claims']['P220'][0]['mainsnak']['datavalue']['value']
+    except:
+        pass
+    languages.append(result)
+
+
+with open('languages_nomatch.json') as f:
+    manual_langs = json.load(f)
+
+languages += manual_langs
+
+for l in languages:
+    l['_unique_name'] = slugify(f'language_{l["name"]}', separator="_")
+    l['dgraph.type'] = "Language"
+    l['uid'] = "_:" + l['_unique_name']
+
+
+mutation = {'set': languages}
+
+with open('data/languages.json', 'w') as f:
+    json.dump(mutation, f, indent=2)
