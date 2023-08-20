f3780fef0fcfd9aa8660a7817c10eefa66da3759
Paul Balluff <paul.balluff@gmail.com>
2023-08-20 21:16:12 +0200

    #24 #3 conceptual work on resolving ambiguous authors

--- flaskinventory/external/doi.py
+++ flaskinventory/external/doi.py
@@ -3,13 +3,15 @@ from typing import Union
 from dateutil import parser as dateparser
 from thefuzz import fuzz
 
-
-def resolve(doi: str) -> Union[dict, bool]:
-    # clean input string
+def clean_doi(doi: str) -> str:
+    """ strips unwanted stuff from DOI strings """
     doi = doi.replace("https://doi.org/", "")
     doi = doi.replace("http://doi.org/", "")
     doi = doi.replace("doi.org/", "")
+    return doi
 
+
+def crossref(doi: str) -> Union[dict, bool]:
 
     api = 'https://api.crossref.org/works/'
 
@@ -161,42 +163,100 @@ def datacite(doi: str) -> dict:
 
     return result
 
+def zenodo(doi: str) -> dict:
+    pass
+
 def orcid_search(name: str=None, family: str= None, given: str=None, affiliation: str = None) -> dict:
     """ Fallback to orcid as last ressort"""
     pass
 
+def resolve_doi(doi: str) -> dict:
+    """ 
+        query a series of APIs and return clean data 
+        Tries in this order
+        1. OpenAlex (most convenient)
+        2. Crossref
+        3. Datacite
+        4. DOI.org
+
+        - Zenodo DOIs are handled by zenodo directly
+        - Tries to get canonical Author IDs
+    """
+    doi = clean_doi(doi)
+
+    if 'zenodo' in doi.lower():
+        result = zenodo(doi)
+
 if __name__ == '__main__':
 
     doi = '10.25522/manifesto.mpdssa.2020b'
+    import json
+    with open('flaskinventory/config.json') as f:
+        config = json.load(f)
+
+    headers = {}
+
+    api = "https://pub.orcid.org/"
+
+    author = "Krause, Werner"
+
+    headers = {
+            "Authorization type": "Bearer",
+            "Access token": config['ORCID_ACCESS_TOKEN'],
+            "Content-type": "application/json",
+            "scope": "/read-public"
+            }
+
+    r = requests.get(api + 'v3.0/expanded-search/', params={'q': 'given-and-family-names:' + author}, headers=headers)
+    # r = requests.get(api + 'v3.0/expanded-search/', params={'q': 'digital-object-ids:"' + doi + '"'}, headers=headers)
+
+    print(r.json())
+    print(r.json()['expanded-result'][:5])
+
+    # take top 10 results
+    candidates = r.json()['expanded-result'][:10]
+    for c in candidates:
+        name = c['given-names'] + ' ' + c['family-names']
+        score = fuzz.partial_token_sort_ratio(name, author)
+        c['name'] = name
+        c['score'] = score
+        # print(name, c['orcid-id'], score)
+
+    highest_score = max(candidates, key=lambda x: x['score'])
+    print('highest', highest_score)
+
+    # check if there are other candidates with same score
+    other_candidates = [c for c in candidates if c['score'] == highest_score]
+    if len(other_candidates) > 0:
+        print(other_candidates) 
 
     # result = datacite(doi)
 
     # print(result)
 
-    affiliation = "WZB Berlin Social Science Center"
+    # affiliation = "WZB Berlin Social Science Center"
 
-    author = "Zehnter, Lisa"
 
-    candidates = openalex_getauthor_ids(author)
+    # candidates = openalex_getauthor_ids(author)
 
-    print(candidates)
+    # # print(candidates)
     
-    for c in candidates:
-        c['x_concepts'] = None
-        score = 0
-        if not 'relevance_score' in c:
-            c['relevance_score'] = 1
-        try:
-            score = fuzz.partial_token_sort_ratio(c['last_known_institution']['display_name'], affiliation)
-            c['institution_match'] = score
-            c['overall_score'] = c['relevance_score'] * score
-            if c['orcid']:
-                c['overall_score'] = c['overall_score'] * 2
-        except:
-            c['overall_score'] = 0
+    # for c in candidates:
+    #     c['x_concepts'] = None
+    #     score = 0
+    #     if not 'relevance_score' in c:
+    #         c['relevance_score'] = 1
+    #     try:
+    #         score = fuzz.partial_token_sort_ratio(c['last_known_institution']['display_name'], affiliation)
+    #         c['institution_match'] = score
+    #         c['overall_score'] = c['relevance_score'] * score
+    #         if c['orcid']:
+    #             c['overall_score'] = c['overall_score'] * 2
+    #     except:
+    #         c['overall_score'] = 0
 
-    from pprint import pprint
+    # from pprint import pprint
 
-    pprint(candidates)
+    # pprint(candidates)
 
     # print(sorted(candidates, key=lambda x: x['overall_score'], reverse=True)[0])
