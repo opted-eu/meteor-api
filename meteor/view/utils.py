470d0a6278b7bc7b0fe0f6cd6084aadc90d7ee8a
Paul Balluff <paul.balluff@gmail.com>
2023-09-14 17:31:22 +0200

    #8 add logic for "revise", add hinting

--- meteor/view/utils.py
+++ meteor/view/utils.py
@@ -1,16 +1,15 @@
-from dateutil import parser as dateparser
-from flask import url_for
+from meteor.users.constants import USER_ROLES
 
 """
 Helper function to evaluate viewing permissions
 Entries with "draft" or "pending" status can only be
 viewed by the user who created the item or reviewers/admins
 """
-def can_view(entry, user):
+def can_view(entry, user) -> bool:
     if "entry_review_status" in entry.keys():
-        if entry.get('entry_review_status') in ['pending', 'rejected']:
+        if entry.get('entry_review_status') in ['pending', 'rejected', 'revise']:
             if user.is_authenticated:
-                if user._role > 1 or entry.get('_added_by').get('uid') == user.id:
+                if user._role >= USER_ROLES.Reviewer or entry['_added_by']['uid'] == user.id:
                     return True
                 else:
                     return False
@@ -18,7 +17,7 @@ def can_view(entry, user):
                 return False
         elif entry.get('entry_review_status') == 'draft':
             if user.is_authenticated:
-                if user._role > 2 or entry.get('_added_by').get('uid') == user.id:
+                if user._role >= USER_ROLES.Admin or entry.get('_added_by').get('uid') == user.id:
                     return True
                 else: 
                     return False
