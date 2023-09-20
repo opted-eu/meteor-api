b98568a571139f7d6655cf14712b2fa2cda7c627
Paul Balluff <paul.balluff@gmail.com>
2023-09-20 21:47:06 +0200

    add unbuffered flag

--- tools/import_wps.sh
+++ tools/import_wps.sh
@@ -2,10 +2,10 @@ echo "Importing Data from Work Packages"
 echo "WP3..."
 ./tools/wp3migrate.sh
 echo "WP4..."
-python3 tools/wp4_import.py
+python3 -u tools/wp4_import.py
 echo "WP5..."
-python3 tools/wp5_import.py
+python3 -u tools/wp5_import.py
 echo "WP2..."
-python3 tools/wp2_import.py
+python3 -u tools/wp2_import.py
 echo "Cleanup ..."
-python3 tools/migration_cleanups.py
+python3 -u tools/migration_cleanups.py
--- tools/wp3migrate.sh
+++ tools/wp3migrate.sh
@@ -1,5 +1,5 @@
 echo "Changing WP3 backup files..."
-python3 tools/alter_backup.py
+python3 -u tools/alter_backup.py
 echo "Sucess!"
 echo "Deleting all Data from DGraph..."
 curl -X POST localhost:8080/alter -d '{"drop_op": "DATA"}'
@@ -8,4 +8,4 @@ echo "Loading altered WP3 backup"
 dgraph live --files data/g01.rdf --schema data/g01.schema --zero localhost:5080
 echo "Success!"
 echo "Running migration scripts..."
-python3 tools/wp3migrate.py
+python3 -u tools/wp3migrate.py
