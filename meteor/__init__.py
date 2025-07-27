a60956a65dc1141eb9334225211edab92c1e85f4
mrwunderbar666 <paul.balluff@gmail.com>
2025-07-27 14:27:47 +0200

    incrememnt version

--- meteor/__init__.py
+++ meteor/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 
 import logging
 import json
@@ -28,13 +28,15 @@ from meteor.flaskdgraph import DGraph
 
 dgraph = DGraph()
 
+
 class AnonymousUser(AnonymousUserMixin):
     _role = 0
     uid = None
 
+
 login_manager = LoginManager()
-login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "users.login"
+login_manager.login_message_category = "info"
 login_manager.anonymous_user = AnonymousUser
 
 
@@ -46,6 +48,7 @@ from meteor.users.authentication import jwt
 from flask.json.provider import DefaultJSONProvider
 import datetime
 
+
 class UpdatedJSONProvider(DefaultJSONProvider):
     def default(self, o):
         if isinstance(o, (datetime.date, datetime.datetime)):
@@ -56,9 +59,10 @@ class UpdatedJSONProvider(DefaultJSONProvider):
 def create_app(config_class=Config, config_json=None):
     # assert versions
     import wtforms
-    assert wtforms.__version__.startswith('3.'), 'WTForms Version 3.X.X is required!'
 
-    app = Flask(__name__, static_url_path='/legacy/static')
+    assert wtforms.__version__.startswith("3."), "WTForms Version 3.X.X is required!"
+
+    app = Flask(__name__, static_url_path="/legacy/static")
     app.json = UpdatedJSONProvider(app)
 
     app.logger.addHandler(create_filehandler())
@@ -68,37 +72,42 @@ def create_app(config_class=Config, config_json=None):
     else:
         app.config.from_object(config_class)
 
-    if 'TESTING' not in app.config:
-        app.config['TESTING'] = False
+    if "TESTING" not in app.config:
+        app.config["TESTING"] = False
 
-    if app.config.get('DEBUG_MODE'):
+    if app.config.get("DEBUG_MODE"):
         app.debug = True
         try:
-            with open('.git/HEAD') as f:
+            with open(".git/HEAD") as f:
                 git = f.read()
-            branch =  git.split('/')[-1].strip()
-            with open('.git/' + git[5:-1]) as f:
+            branch = git.split("/")[-1].strip()
+            with open(".git/" + git[5:-1]) as f:
                 commit = f.read()
-            global __version__ 
-            __version__ += '-' + branch + '-' + commit[:7]
+            global __version__
+            __version__ += "-" + branch + "-" + commit[:7]
         except:
-            pass 
-    
+            pass
+
     if app.debug:
         app.logger.setLevel(logging.DEBUG)
-    
-    if app.config.get('SLACK_LOGGING_ENABLED'):
-        try:
-            slack_handler = create_slackhandler(app.config.get('SLACK_WEBHOOK'))
-            app.logger.addHandler(slack_handler)
-            app.logger.error('Initialized Slack Logging!')
-        except Exception as e:
-            app.logger.error(f'Slack Logging not working: {e}')
 
-    app.config['APP_VERSION'] = __version__
-    
-    cors = CORS(app , resources={r"/*": {"origins": "*", "allow_headers": "*", "expose_headers": "*"}})
-    
+    if app.config.get("SLACK_LOGGING_ENABLED"):
+        try:
+            slack_handler = create_slackhandler(app.config.get("SLACK_WEBHOOK"))
+            app.logger.addHandler(slack_handler)
+            app.logger.error("Initialized Slack Logging!")
+        except Exception as e:
+            app.logger.error(f"Slack Logging not working: {e}")
+
+    app.config["APP_VERSION"] = __version__
+
+    cors = CORS(
+        app,
+        resources={
+            r"/*": {"origins": "*", "allow_headers": "*", "expose_headers": "*"}
+        },
+    )
+
     if app.debug:
         from meteor.users.routes import users
         from meteor.view.routes import view
@@ -108,14 +117,15 @@ def create_app(config_class=Config, config_json=None):
         from meteor.endpoints.routes import endpoint
         from meteor.main.routes import main
         from meteor.errors.handlers import errors
-        app.register_blueprint(users, url_prefix='/legacy')
-        app.register_blueprint(view, url_prefix='/legacy')
-        app.register_blueprint(add, url_prefix='/legacy')
-        app.register_blueprint(edit, url_prefix='/legacy')
-        app.register_blueprint(review, url_prefix='/legacy')
-        app.register_blueprint(endpoint, url_prefix='/legacy')
-        app.register_blueprint(main, url_prefix='/legacy')
-        app.register_blueprint(errors, url_prefix='/legacy')
+
+        app.register_blueprint(users, url_prefix="/legacy")
+        app.register_blueprint(view, url_prefix="/legacy")
+        app.register_blueprint(add, url_prefix="/legacy")
+        app.register_blueprint(edit, url_prefix="/legacy")
+        app.register_blueprint(review, url_prefix="/legacy")
+        app.register_blueprint(endpoint, url_prefix="/legacy")
+        app.register_blueprint(main, url_prefix="/legacy")
+        app.register_blueprint(errors, url_prefix="/legacy")
 
     jwt.init_app(app)
 
@@ -126,8 +136,11 @@ def create_app(config_class=Config, config_json=None):
     # csrf = CSRFProtect(app)
 
     from meteor.api.routes import api
-    app.register_blueprint(api, url_prefix='/api')
 
-    Markdown(app, extensions=[TocExtension(baselevel=3, anchorlink=True), 'fenced_code'])
+    app.register_blueprint(api, url_prefix="/api")
+
+    Markdown(
+        app, extensions=[TocExtension(baselevel=3, anchorlink=True), "fenced_code"]
+    )
 
     return app
